fb87f88a8c91094903ef46bb8a72afda49e96113
Mohamad <mohammad.helehel@hotmail.com>
2023-09-29 15:29:38 +0200

    Second

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start on app!</Text>
+      <Text>Mera Text</Text>
       <StatusBar style="auto" />
     </View>
   );
